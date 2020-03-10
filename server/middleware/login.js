7d92d4a35fe5c517bc3662d3bb99673853f5e5de
Gianluca Donato <gian.dnt@gmail.com>
2020-03-10 12:02:26 +0100

    v1.0

---
+++ server/middleware/login.js
@@ -0,0 +1,22 @@
+module.exports = (db) => async (req, res) => {
+  const Players = db.collection('players');
+  const { name } = req.body;
+
+  let player = await Players.findOne({ name });
+  if (player) {
+    if (player.online) {
+      return res.status(401).json({ error: 'Player already online' });
+    }
+  } else {
+    // create new player
+    const avatar = `/monster/${Math.ceil(Math.random() * 50) + 1}.png`;
+    ({ ops: [player] } = await Players.insertOne({
+      name,
+      avatar,
+      lives: 3,
+      shots: 0,
+      online: true,
+    }));
+  }
+  return res.json(player);
+};
